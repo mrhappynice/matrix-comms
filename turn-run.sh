cf92d832e616155de48428c72753a690d3383fe8
Mr. Happy Nice <themrhappynice@gmail.com>
2025-09-16 00:47:07 -0500

    Update turn-run.sh

--- turn-run.sh
+++ turn-run.sh
@@ -1,11 +1,11 @@
 docker run -d --name coturn --restart unless-stopped \
   --network host \
-  -v /etc/letsencrypt/live/matrix.example.site:/certs:ro \
+  -v /etc/letsencrypt/live/matrix.example.com:/certs:ro \
   instrumentisto/coturn \
   -n --no-cli --no-tls --no-dtls \
   --listening-port=3478 \
   --min-port=49152 --max-port=49999 \
-  --realm=example.site \
+  --realm=example.com \
   --use-auth-secret --static-auth-secret=CHANGE_ME_SHARED_SECRET \
   --no-tcp-relay \
   --external-ip="$(curl -s ifconfig.me)" \
